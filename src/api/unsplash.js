c1dabe2214e23f00284378b3647c8e55b4252483
Pratik Kenaudekar <scorp86@gmail.com>
2022-01-06 14:10:55 +0530

    Change to .env files

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 const keys = require('../config/keys');
+const ACCESS_KEY = process.env.REACT_APP_ACCESS_KEY;
 
 export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
-    Authorization: `Client-ID ${process.env.REACT_APP_ACCESS_KEY}`,
+    Authorization: 'Client-ID ' + ACCESS_KEY,
   },
 });
--- src/config/prod.js
+++ src/config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  accessKey: process.env.ACCESS_KEY,
-  secretKey: process.env.SECRET_KEY,
+  accessKey: process.env.REACT_APP_ACCESS_KEY,
+  secretKey: process.env.REACT_APP_SECRET_KEY,
 };
